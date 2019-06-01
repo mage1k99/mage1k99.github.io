3226f1a4d1db009153ad5775623fb589e31a1b7d
Mac <mage1k99@gmail.com>
2019-06-01 23:09:08 +0530

    Fixed nav. bar scroll color change and added portfolio image

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,16 +7,12 @@ function burgerFunction(){
       }
 }
 
-// chance the navigator. bar color while scrolling
-
-    window.scroll = function () { 
-        myFunction();
-    }
-    function myFunction(){
+    window.onscroll = function() {myFunction()};
+    function myFunction() {
         var navbar = document.getElementById("navbar");
-        if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
-            navbar.className = "navbar-fixed-top";
-        }else{
-            navbar.className = "navbar"
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            navbar.className = "navbar" + " navbar-fixed";
+        } else {
+            navbar.className = navbar.className.replace(" navbar-fixed", "");
         }
     }
