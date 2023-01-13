70b6a3f50910aa43b32bb60148ef3e678f4ebde1
MageshBabu K <mage1k99@fossfreaks.com>
2023-01-13 19:14:24 +0530

    fix grammatical correctness

--- pages/index.vue
+++ pages/index.vue
@@ -12,24 +12,24 @@ useHead({
       <div class="prose">
         <h1 class="text-center">Hey There 👋</h1>
         <p>
-          I am MageshBabu, a Software Developer working in
+          I am MageshBabu, a Software Developer working at
           <a href="https://fossfreaks.com">FossFreaks</a>
         </p>
         <p>
-          I work on different kinds of projects and various tech stack. Created
+          I work on different kinds of projects and various tech stacks. Created
           projects, most of them are private for now. New projects are yet to
-          come. I kinda like simple but yet aesthetic design so this is why I
-          choose this design for this.
+          come. I choose a simple yet aesthetic design, that's the reason for
+          this design.
         </p>
         <p>
-          Also planning to create a blog with this site. soon will be updating
-          this site. You can always say hello by using one of
+          Also planning to create a blog with this site. I will be updating this
+          site. You can always say hello by using one of
           <nuxt-link to="contact">these</nuxt-link>
         </p>
         <p>
-          Other than programming, I also enjoy listening to music, Spending time
-          with Loved ones and my pets. I'm a person who seeks bit of privacy,
-          which is why I'm not sharing all my details here. you can contact me
+          Apart from programming, I also enjoy listening to music, Spending time
+          with Loved ones and my pets. I'm a person who seeks a bit of privacy,
+          which is why I'm not sharing all my details here. You can contact me
           via my handles. Thank you for Spending time reading this, Have a nice
           day.
         </p>
