4c141ee281222d0ad0b4110cb2790b215127b23f
MageshBabu K <mage1k99@gmail.com>
2024-08-10 19:07:04 +0530

    show random emojis at footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,43 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const emojis = [
+  "🌊",
+  "🎉",
+  "🦄",
+  "🎁",
+  "🎋",
+  "🎈",
+  "🎧",
+  "🎸",
+  "🎺",
+  "🎻",
+  "🎶",
+  "🎵",
+  "🎭",
+  "🚀",
+  "🌈",
+  "🎨",
+  "🧑‍🎤",
+];
+
+const randomEmoji = ref(emojis[Math.floor(Math.random() * emojis.length)]);
+
+onMounted(() => {
+  const link = document.createElement("link");
+  link.setAttribute("rel", "stylesheet");
+  link.setAttribute(
+    "href",
+    "https://fonts.googleapis.com/css2?family=Noto+Color+Emoji&display=swap"
+  );
+  document.head.appendChild(link);
+});
+</script>
+
 <template>
   <footer>
     <div class="container mx-auto">
-      <p class="text-gray-700 text-center text-sm">🌚</p>
+      <p class="text-gray-700 text-center text-lg">{{ randomEmoji }}</p>
     </div>
   </footer>
 </template>
